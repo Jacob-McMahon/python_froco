b4be751daabc5993011106b8349b4fbae8fcb3d9
Jacob McMahon <jacobryanmcmahon@gmail.com>
2024-01-30 10:18:17 +0100

    Got bs4 working slightly need to now take tag.contents and find the sum of all of them

--- span_finder.py
+++ span_finder.py
@@ -1,6 +1,21 @@
+import urllib.request, urllib.parse, urllib.error
+import sys 
+sys.path.append("C:\Python311\Lib\site-packages")
+from bs4 import BeautifulSoup
+import ssl
 
-# Retrieve all of the anchor tags
-tags = 1 #soup('span')
+#adding code to bypass SSL cert errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = (input('Enter something: '))
+html = urllib.request.urlopen(url, context = ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+
+# Retrieve all of the span tags
+tags = soup('span')
 for tag in tags:
    # Look at the parts of a tag
    print ('TAG:',tag)
